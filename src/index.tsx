ce7cf214e301d5424b8b8ba500fcc35758d19d59
StevanSkrbic <stevan.skrbic@htecgroup.com>
2020-11-21 20:35:32 +0100

    - finished to-do list with recoil

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-
+import {RecoilRoot} from 'recoil'
+// There is only one recoil root per app and everything within recoil state exists under this recoil root provider
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>
     </React.StrictMode>,
     document.getElementById('root'),
 )
